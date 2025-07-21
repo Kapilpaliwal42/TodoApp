6324374a90e48941feebc93b427bc8a105a276c2
Kapilpaliwal42 <162558180+Kapilpaliwal42@users.noreply.github.com>
2025-07-21 14:05:38 +0530

    Update login.jsx

--- frontend/login_Api/src/login.jsx
+++ frontend/login_Api/src/login.jsx
@@ -15,7 +15,7 @@ export default function Login({ onSwitchToSignup, onLoginSuccess }) {
     setError('');
 
     try {
-      const response = await fetch('http://localhost:3000/api/auth/login/', {
+      const response = await fetch('import.meta.env.VITE_BACKEND_API_URL/api/auth/login/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
